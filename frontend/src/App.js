308031b125126df0074065e7b57f24359bade627
Uniunt🍥⚓ <106765520+Uniunt@users.noreply.github.com>
2022-08-05 15:46:41 +0800

    simple interactive APP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,42 @@
-/*import logo from './logo.svg';*/
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment count</button>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+/*import logo from './logo.svg';
 import './App.css';
 
-/*function App() {
+function App() {
   return (
     <div className="App">
       <h1>Welcome to CISE - the home of learning and fun</h1>
         </div>
         );
-      } */
+      }
 
 /*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,11 +51,11 @@ import './App.css';
         >
           Learn React
         </a>
-        </header>*/ 
+        </header>
 
 const App = () =>
 <div ClassName = "App">
   <h1>Welcome to CISE - the home of learning and fun</h1>
 </div>
 
-export default App;
+export default App;*/
